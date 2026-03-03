d2c20f433afba16c1f9749ca5f3ea1e2fa49942f
Isaac Mcknew <imcknew@ut.utm.edu>
2026-03-02 21:59:29 -0600

    basic header file made (need to double check later)

--- stacker.h
+++ stacker.h
@@ -10,8 +10,56 @@
 #ifndef STACKER_H
 #define STACKER_H
 
-class stacker {
+#include <vector>
 
+class stacker {
+Private:
+
+  //magic number is the p3 value
+  string magic_number;
+  //width and height describe the image dimension
+  //max_color is the color depth
+  int width, height, max_color;
+
+  //struct holding the color values of each color
+  struct pixel{
+    int red;
+    int green;
+    int blue;
+  }
+
+  //vector of pixel structs
+  vector<struct> pixels;
+
+Public:
+  
+
+/**
+ * constructor
+ *
+ * @pre  
+ * @post 
+ * 
+ */
+  stacker();
+
+  //copy constructor
+  stacker(const stacker& other);
+
+  //destructor
+  ~stacker();
+
+
+/**
+ * puts the pixels into the vector. Could probably do that in the constructor?
+ *
+ * @pre 
+ * @return vector<struct> pixels
+ * @post the vector of pixels
+ * 
+ */
+  vector<struct> scan();
+  
 
 
 };
