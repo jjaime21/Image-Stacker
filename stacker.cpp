a734e8ab1c48332c486ae82ccf89d12c68e295dc
Isaac Mcknew <imcknew@ut.utm.edu>
2026-03-06 23:12:42 -0600

    added to readimage before bed

--- stacker.cpp
+++ stacker.cpp
@@ -32,7 +32,18 @@ stacker::stacker(){
 
 
 void stacker::readFile(const std::string& image, int numberImages){
+  ifstream inputFile(image);
 
+  if(!inputFile.is_open()){
+    cout << "Please choose a valid file" << endl;
+  }
+
+  inputFile >> magic_number >> width >> height >> max_color;
+  pixels.reserve(width * height);
+
+  //find way to change the pixel struct so it can be put into the pixels vector
+
+  inputFile.close();
 
 }
 
